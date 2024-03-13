603d216e15b011a94d08d0a9369f11a6ba46c515
armin zaugg <aza@artemis.local>
2024-03-13 17:46:22 +0100

    add Header in Config Dialog

--- src/index.jsx
+++ src/index.jsx
@@ -1,53 +1,66 @@
-import ForgeUI, {render, Text, AssetsAppImportTypeConfiguration, useProductContext, Button} from '@forge/ui';
-import {controllerQueueHandler, controllerQueue} from './controller-resolver';
-import {workerQueueHandler} from './worker-resolver';
-import api, {route, storage, startsWith} from '@forge/api';
+import ForgeUI, {
+  render,
+  Text,
+  AssetsAppImportTypeConfiguration,
+  useProductContext,
+  Button,
+  Heading,
+} from "@forge/ui";
+import { controllerQueueHandler, controllerQueue } from "./controller-resolver";
+import { workerQueueHandler } from "./worker-resolver";
+import api, { route, storage, startsWith } from "@forge/api";
 
-import { mapping } from './schema';
-import { deleteImportAndMapping, patchMapping } from './utils';
+import { mapping } from "./schema";
+import { deleteImportAndMapping, patchMapping } from "./utils";
 
-export {onDeleteImport, startImport, stopImport, importStatus, controllerQueueHandler, workerQueueHandler};
+export {
+  onDeleteImport,
+  startImport,
+  stopImport,
+  importStatus,
+  controllerQueueHandler,
+  workerQueueHandler,
+};
 
 const onDeleteImport = async (context) => {
-  console.log('import with id ', context.importId + ' got deleted');
+  console.log("import with id ", context.importId + " got deleted");
 
   return {
-    result: 'on delete import'
+    result: "on delete import",
   };
 };
 
 const startImport = async (context) => {
-  console.log('import with id ', context.importId + ' got started');
-  
+  console.log("import with id ", context.importId + " got started");
 
   // Check if an import is already running
   const configStatus = await api
-  .asUser()
-  .requestJira(
-    route`/jsm/assets/workspace/${context.workspaceId}/v1/importsource/${context.importId}/configstatus`,
-    {
-      method: "GET",
-    }
-  );
-  console.log('configStatus', configStatus);
-  const configJson = await configStatus.json();  
+    .asUser()
+    .requestJira(
+      route`/jsm/assets/workspace/${context.workspaceId}/v1/importsource/${context.importId}/configstatus`,
+      {
+        method: "GET",
+      },
+    );
+  console.log("configStatus", configStatus);
+  const configJson = await configStatus.json();
 
   // If an import is already running, stop it
-  if (configJson.status === 'RUNNING') {
+  if (configJson.status === "RUNNING") {
     // Extract executionId from cancelUrl
-    const cancelUrl = configJson.links.cancel
-    let executionId = cancelUrl.split('/').pop();
+    const cancelUrl = configJson.links.cancel;
+    let executionId = cancelUrl.split("/").pop();
     const stopImport = await api
       .asUser()
       .requestJira(
         route`/jsm/assets/workspace/${context.workspaceId}/v1/importsource/${context.importId}/executions/${executionId}`,
         {
           method: "DELETE",
-        }
+        },
       );
-    console.log('stopImport', stopImport);
+    console.log("stopImport", stopImport);
   }
-  
+
   // Create a new execution
   const newlyCreatedExecution = await api
     .asUser()
@@ -55,55 +68,60 @@ const startImport = async (context) => {
       route`/jsm/assets/workspace/${context.workspaceId}/v1/importsource/${context.importId}/executions`,
       {
         method: "POST",
-      }
+      },
     );
-  console.log('newlyCreatedExecution', newlyCreatedExecution);
+  console.log("newlyCreatedExecution", newlyCreatedExecution);
   const newlyCreatedExecutionJson = await newlyCreatedExecution.json();
-  let executionId = newlyCreatedExecutionJson.links.cancel.split('/').pop();
+  let executionId = newlyCreatedExecutionJson.links.cancel.split("/").pop();
   debugger;
-  
+
   // Write Import Metadata to storage
   await storage.set(`import-context`, {
     importId: context.importId,
     workspaceId: context.workspaceId,
-    executionId: executionId
+    executionId: executionId,
   });
 
   // Push event onto controller queue to start data ingestion process
-  const id = await controllerQueue.push({eventContext: {importConfigurationId: context.importId}});
+  const id = await controllerQueue.push({
+    eventContext: { importConfigurationId: context.importId },
+  });
   console.log(`Pushed queueControllerEvent with id ${id}`);
 
   return {
-    result: 'start import'
+    result: "start import",
   };
 };
 
 const stopImport = async (context) => {
-  console.log('import with id ', context.importId + ' got stopped');
+  console.log("import with id ", context.importId + " got stopped");
 
   return {
-    result: 'stop import'
+    result: "stop import",
   };
 };
 
 const importStatus = async (context) => {
-  const status = 'READY';
+  const status = "READY";
 
-  console.log(`import with id `, context.importId + ` sending import progress ${status}`);
+  console.log(
+    `import with id `,
+    context.importId + ` sending import progress ${status}`,
+  );
 
   return {
-    status: status
+    status: status,
   };
 };
 
 const App = () => {
-  const {extensionContext} = useProductContext();
+  const { extensionContext } = useProductContext();
   const importId = extensionContext.importId;
   const workspaceId = extensionContext.workspaceId;
+  debugger;
   const onSubmit = async () => {
-    console.log('submit button clicked, submitting schema to import source...');
-    console.log('###\n', mapping);
-    debugger;
+    console.log("submit button clicked, submitting schema to import source...");
+    console.log("###\n", mapping);
     const response = await api
       .asUser()
       .requestJira(
@@ -112,27 +130,29 @@ const App = () => {
           method: "PUT",
           body: JSON.stringify(mapping),
           headers: {
-            "Accept": "application/json",
+            Accept: "application/json",
             "Content-Type": "application/json",
           },
-        }
+        },
       );
     // Write response to console
     console.log(await response.json());
     if (response.status === 204) {
-      console.log('schema submitted successfully');
+      console.log("schema submitted successfully");
     }
     if (response.status === 409) {
-      console.log('A mapping already exists for this import');
+      console.log("A mapping already exists for this import");
     }
   };
 
   return (
     <AssetsAppImportTypeConfiguration onSubmit={onSubmit}>
+      <Heading size="large">Netbox Importer Config</Heading>
       <Text>
-        Hello World!, ImportId = {importId}, WorkspaceId = {workspaceId}
+        Netbox Importer App Config, ImportId = {importId}, WorkspaceId ={" "}
+        {workspaceId}
       </Text>
     </AssetsAppImportTypeConfiguration>
   );
 };
-export const renderImportConfig = render(<App/>);
+export const renderImportConfig = render(<App />);
